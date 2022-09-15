aa323e08653b475b39d95a4b7f59b4f5c62d74a6
Eric Zhan <mightyerwa@gmail.com>
2022-09-15 12:55:05 -0500

    Second page in progress
    Creating the second page, and currentylyworking on switching screens

--- app/src/main/java/com/example/stylesandthemes/MainActivity.java
+++ app/src/main/java/com/example/stylesandthemes/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.stylesandthemes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void switchScreens(View view)
+    {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 
 }
