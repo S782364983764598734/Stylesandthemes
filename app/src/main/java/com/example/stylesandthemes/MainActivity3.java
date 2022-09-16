c20d9bd33ad2741ac62a11456effbe0202367d00
Eric Zhan <mightyerwa@gmail.com>
2022-09-16 12:24:48 -0500

    Page Switch
    Finished The code to switch between pages, fixed the mistake of making a TV screen instead of empty activity.

--- app/src/main/java/com/example/stylesandthemes/MainActivity3.java
+++ app/src/main/java/com/example/stylesandthemes/MainActivity3.java
@@ -1,25 +1,21 @@
 package com.example.stylesandthemes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
     }
-
-    public void switchScreens(View view)
+    public void switchBack(View view)
     {
-        Intent intent = new Intent(this, MainActivity3.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-
 }
